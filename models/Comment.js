62cbda96b8d83273ba5c2a0197a6de63126e37de
Somi Gupta <somigupta@sudharshans-MacBook-Pro.local>
2021-04-28 23:20:47 -0400

    Added seeds to update data

---
+++ models/Comment.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    comment_text: {
+        type: DataTypes.STRING,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    },
+    blog_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'blog',
+            key: 'id'
+        } 
+    }  
+},
+{
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+}
+);
+
+module.exports = Comment;
