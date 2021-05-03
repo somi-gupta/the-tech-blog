ad10c142967301429e7cfc781a133b278be261c8
Somi Gupta <somigupta@sudharshans-MacBook-Pro.local>
2021-05-03 17:34:59 -0400

    Few fixes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,24 +2,30 @@ const { Blog, User, Comment } = require('../models');
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
     try {
-      // Get all projects and JOIN with user data
+      // Get all blogs and JOIN with user data
       const blogData = await Blog.findAll({
-        // include: [
-        //   {
-        //     model: User,
-        //     attributes: ['name'],
-        //   },
-        //   {
-        //     model: Comment,
-        //     attributes: ['id', 'comment_text', 'blog_id', 'user_id', 'created_at'],
-        //     include: {
-        //         model: User,
-        //         attributes: ['username']
-        //     }
-        // } 
-        // ],
+        attributes: [
+          'id',
+          'title',
+          'content',
+          'created_at'
+      ],
+        include: [
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'blog_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['name']
+            }
+        },
+        {
+          model: User,
+          attributes: ['name'],
+        }, 
+        ],
       });
 
     // Serialize data so the template can read it
@@ -38,12 +44,78 @@ router.get('/', async (req, res) => {
 // Login route
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect to the homepage
-  if (req.session.loggedIn) {
+  if (req.session.logged_in) {
     res.redirect('/');
     return;
   }
   // Otherwise, render the 'login' template
   res.render('login');
+});
+
+router.get('/blog/:id',  async(req, res) => {
+  const singleBlog = await Blog.findOne({
+    where: {
+      id: req.params.id
+  },
+  attributes: [
+      'id',
+      'content',
+      'title',
+      'created_at'
+  ],
+  include: [{
+          model: Comment,
+          attributes: ['id', 'comment_text', 'blog_id', 'user_id', 'created_at'],
+          include: {
+              model: User,
+              attributes: ['name']
+          }
+      },
+      {
+          model: User,
+          attributes: ['name']
+      }
+  ]
+  })
+  const blog = singleBlog.get({ plain: true });
+  console.log(blog);
+  res.render('blog-id', { blog, logged_in: req.session.logged_in });
+  
+});
+
+//Route to post comments on the blog
+router.get('/addcomments', async(req,res) => {
+  const singleComment = Blog.findOne({
+    where: {
+        id: req.params.id
+    },
+    attributes: [
+        'id',
+        'content',
+        'title',
+        'created_at'
+    ],
+    include: [{
+            model: Comment,
+            attributes: ['id', 'comment_text', 'blog_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['name']
+            }
+        },
+        {
+            model: User,
+            attributes: ['name']
+        }
+    ]
+})
+if(!singleComment){
+  res.status(404).json({ message: 'No blog found with this id' });
+  return;
+}
+const blogComment = (await singleComment).get({plain:true});
+res.render('blog.comments', { blogComment, logged_in: req.session.logged_in });
+
 });
 
 module.exports = router;
