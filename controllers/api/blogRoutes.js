8914c59a9230e6322becb97f06bf189cae4b8ddd
Somi Gupta <somigupta@sudharshans-MacBook-Pro.local>
2021-05-03 21:48:13 -0400

    Added async await

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -3,73 +3,73 @@ const { Blog, User, Comment } = require('../../models');
 
 const withAuth = require('../../utils/auth');
 
-router.get('/', (req, res) => {
-  console.log('======================');
-  Blog.findAll({
-          attributes: ['id',
-              'title',
-              'content',
-              'created_at'
-          ],
-          order: [
-              ['created_at', 'DESC']
-          ],
-          include: [{
-                  model: User,
-                  attributes: ['name']
-              },
-              {
-                  model: Comment,
-                  attributes: ['id', 'comment_text', 'blog_id', 'user_id', 'created_at'],
-                  include: {
-                      model: User,
-                      attributes: ['name']
-                  }
-              }
-          ]
-      })
-      .then(dbPostData => res.json(dbPostData.reverse()))
-      .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-      });
+router.get('/', async (req, res) => {
+  try {
+    const blogData = await Blog.findAll({
+        attributes: ['id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        order: [
+            ['created_at', 'DESC']
+        ],
+        include: [{
+                model: User,
+                attributes: ['name']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'blog_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['name']
+                }
+            }
+        ]
+    })
+    res.json(blogData.reverse())
+  } catch(err){
+    console.log(err.message);
+    res.status(500).json(err.message);
+  }
 
 });
-router.get('/:id', (req, res) => {
-  Blog.findOne({
-          where: {
-              id: req.params.id
-          },
-          attributes: ['id',
-              'content',
-              'title',
-              'created_at'
-          ],
-          include: [{
-                  model: User,
-                  attributes: ['name']
-              },
-              {
-                  model: Comment,
-                  attributes: ['id', 'comment_text', 'blog_id', 'user_id', 'created_at'],
-                  include: {
-                      model: User,
-                      attributes: ['name']
-                  }
-              }
-          ]
-      })
-      .then(blogData => {
-          if (!blogData) {
-              res.status(404).json({ message: 'No blog found with this id' });
-              return;
-          }
-          res.json(blogData);
-      })
-      .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-      });
+
+router.get('/:id', async (req, res) => {
+    try {
+        const singleBlog = await Blog.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id',
+                'content',
+                'title',
+                'created_at'
+            ],
+            include: [{
+                    model: User,
+                    attributes: ['name']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'blog_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['name']
+                    }
+                }
+            ]
+        });
+        if (!singleBlog) {
+            res.status(404).json({ message: 'No blog found with this id' });
+            return;
+        }
+        res.json(singleBlog);
+    }catch(err){
+          console.log(err.message);
+          res.status(500).json(err.message);
+      }
 });
 
   router.post('/', withAuth, (req, res) => {
